35df1f4c4201ccb8dc12a84f97ad0ab6c96aed61
Gregor <gregorgajic.spv@gmail.com>
2020-12-08 22:01:45 +0100

    day5

---
+++ day5.py
@@ -0,0 +1,34 @@
+# part 1
+with open("day5.txt") as df:
+    passes = df.read().splitlines()
+
+seats = []
+
+for bpass in passes:
+    row=list(range(128))
+    column=list(range(8))
+
+    for r in bpass[0:7]:
+        if r == "F":
+            row=row[0:int((len(row)+1)/2)]
+        elif r == "B":
+            row=row[int((len(row)+1)/2):len(row)+1]
+
+    for c in bpass[7:10]:
+        if c == "L":
+            column=column[0:int((len(column)+1)/2)]
+        elif c == "R":
+            column=column[int((len(column)+1)/2):len(column)+1]
+
+    ID = 8*int(row[0]) + int(column[0])
+
+    seats.append((row, column, ID))
+
+print(f"The maximal ID is {str(max([i[2] for i in seats]))}")
+
+# part 2
+all_IDs = [i[2] for i in seats]
+possible_IDs = [i+1 for i in all_IDs] + [i-1 for i in all_IDs]
+my_ID = set(possible_IDs) - set(all_IDs)
+IDs = ", ".join([str(e) for e in my_ID])
+print(f"The seat ID is one of {IDs}.")
